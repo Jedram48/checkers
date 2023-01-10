4299ab5594b23d9c5d0e0c9504eb5380ae7222da
krymus <krystianmusialczyk@gmail.com>
2023-01-10 21:22:46 +0100

    network works!

--- src/main/java/Network/Server.java
+++ src/main/java/Network/Server.java
@@ -1,69 +1,103 @@
 package Network;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.ArrayList;
 
 public class Server {
-    private static final int port = 4444;
 
-    public static void main(String [] args) throws IOException {
-        boolean wMove = true;
-        ServerSocket listener = new ServerSocket(port);
+    private ServerSocket serverSocket;
+    Socket white;
+    BufferedReader inW;
+    BufferedWriter outW;
+    Socket black;
+    BufferedReader inB;
+    BufferedWriter outB;
 
-        Socket socketW = listener.accept();
-        System.out.println("First Client connected!");
-//        Socket socketB = listener.accept();
-//        System.out.println("Second Client connected!");
-
-        BufferedReader inW = new BufferedReader(new InputStreamReader(socketW.getInputStream()));
-//        BufferedReader inB = new BufferedReader(new InputStreamReader(socketB.getInputStream()));
-        PrintWriter outW = new PrintWriter(socketW.getOutputStream(),true);
-//        PrintWriter outB = new PrintWriter(socketB.getOutputStream(), true);
-
-        outW.println("You are playing White!");
-//        outB.println("You are playing Black!");
-
-
-        for(int i = 0; i < 10; i++){
-            String str = inW.readLine();
-            System.out.println(str);
-        }
-/*
-        game = new Game();
-
-        while(true)
-        {
-            if(wMove)
-            {
-                outW.println("Your turn!");
-                outB.println("Waiting for White...");
-                String s = inW.readLine();
-                System.out.println(s);
-                outB.println("White moved " + s);
-            }
-            else
-            {
-                outB.println("Your turn!");
-                outW.println("Waiting for Black...");
-                String s = inB.readLine();
-                if(s == null) break;
-                System.out.println(s);
-                outW.println("Black moved " + s);
-            }
-            wMove = !wMove;
-        }
-*/
-//        inB.close();
-        inW.close();
-//        outB.close();
-        outW.close();
-//        socketB.close();
-        socketW.close();
-        }
+    public Server(ServerSocket serverSocket) throws IOException
+    {
+        this.serverSocket = serverSocket;
+        System.out.println("Waiting for players to connect...");
+        this.white = serverSocket.accept();
+        this.black = serverSocket.accept();
+        inW = new BufferedReader(new InputStreamReader(white.getInputStream()));
+        inB = new BufferedReader(new InputStreamReader(black.getInputStream()));
+        outW = new BufferedWriter(new OutputStreamWriter(white.getOutputStream()));
+        outB = new BufferedWriter(new OutputStreamWriter(black.getOutputStream()));
     }
 
+    public void startTheGame() throws IOException
+    {
+        listenToWhite();
+        listenToBlack();
+    }
+
+    public void listenToWhite()
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    while(true)
+                    {
+                        String msg = inW.readLine();
+                        //try to update game using msg from white
+                        //if gamestate updated v
+                        broadcastGameState();
+                    }
+                } catch (IOException e)
+                {
+                    System.err.println("Problem with White's connection...");
+                }
+
+            }
+        }).start();
+    }
+
+    public void listenToBlack()
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    while(true)
+                    {
+                        String msg = inB.readLine();
+                        //try to update game using msg from black
+                        //if gamestate updated v
+                        broadcastGameState();
+                    }
+                } catch (IOException e)
+                {
+                    System.err.println("Problem with Black's connection...");
+                }
+
+            }
+        }).start();
+    }
+
+    public void broadcastGameState() throws IOException
+    {
+        //getGameState()
+
+        outW.write("~~Gamestate~~");
+        outW.newLine();
+        outW.flush();
+
+        outB.write("~~Gamestate~~");
+        outB.newLine();
+        outB.flush();
+    }
+
+
+    public static void main(String[] args) throws IOException
+    {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startTheGame();
+
+    }
+}
