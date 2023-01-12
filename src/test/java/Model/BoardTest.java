89897e21b0f9e051f20050306b063b569cbcdcd4
krymus <krystianmusialczyk@gmail.com>
2023-01-12 23:29:13 +0100

    Simple board tests

--- src/test/java/Model/BoardTest.java
+++ src/test/java/Model/BoardTest.java
@@ -1,6 +1,22 @@
 package Model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class BoardTest {
-  
+
+    @Test
+    void simpleBoardTests()
+    {
+        Board board = new Board(5, 4, true);
+        assertEquals(4, board.sizeY);
+        assertEquals(5, board.sizeX);
+        assertEquals(Color.WHITE, board.Fields[0][0].color);
+        assertEquals(Color.BLACK, board.Fields[4][3].color);
+
+        Board board2 = new Board(5, 4, false);
+        assertEquals(Color.BLACK, board2.Fields[0][0].color);
+        assertEquals(Color.WHITE, board2.Fields[4][3].color);
+    }
+
 }
